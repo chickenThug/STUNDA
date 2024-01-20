d6b5fb4810f22a1a8f945dc6f5ff13b98c148c4f
Viggo Svärdkrona <viggos@kth.se>
2024-01-20 16:35:35 +0100

    simple processing and some more EDA

---
+++ src/initial_clean.py
@@ -0,0 +1,82 @@
+import json
+import pandas as pd
+import re
+
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+
+path = "../data/"
+file = "stunda-terms.jsonl"
+working_file = "stunda-terms-processed.jsonl"
+
+nltk.download('wordnet')
+
+def read_jsonl_file(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Load each line as a JSON object
+            json_object = json.loads(line)
+            data.append(json_object)
+    return data
+
+
+data = read_jsonl_file(path + file)
+
+simple_data = []
+
+
+for entry in data:
+    if entry["status"] == "1":
+        simple_data.append(entry)
+        continue
+
+    # pattern to match single word
+    pattern_single_word = r"^[a-z]+$"
+
+    pattern_multiple_words = r"^[a-z]+ ([a-z]+ )*[a-z]+$"
+
+    if re.match(pattern_single_word, entry["eng"]["lemma"]) and re.match(pattern_single_word, entry["swe"]["lemma"]):
+        simple_data.append(entry)
+
+    elif re.match(pattern_multiple_words, entry["eng"]["lemma"]) and re.match(pattern_multiple_words, entry["swe"]["lemma"]):
+        english_words = entry["eng"]["lemma"].split(" ")
+        swedish_words = entry["swe"]["lemma"].split(" ")
+
+        if len(english_words) == len(swedish_words):
+            english_lemma = english_words[0]
+
+            lemmatizer = WordNetLemmatizer()
+            
+            for pos in ["n", "v", "a"]:
+                good = True
+                for word in english_words[1:]:
+                    lemma = lemmatizer.lemmatize(word, pos) 
+                    if english_lemma != lemma:
+                        good = False
+                
+                if good:
+                    entry["eng"]["lemma"] = english_lemma
+                    entry["swe"]["lemma"] = swedish_words[0]
+                    entry["eng"]["inflection"] = english_words[1:]
+                    entry["swe"]["inflection"] = swedish_words[1:]
+                    
+                    entry["pos"] = pos
+                    simple_data.append(entry)
+                    break
+                
+
+
+print(len(simple_data))
+
+with open(path + working_file, 'w') as jsonl_file:
+    for json_object in simple_data:
+        jsonl_file.write(json.dumps(json_object) + '\n')
+
+
+        
+
+
+
