123dfd7faf7e36f8775ee460d596c8d84b7eb574
Viggo Svärdkrona <viggos@kth.se>
2024-01-27 17:11:01 +0100

    wrote function to pos for engilsh words

---
+++ src/scripts/utils.py
@@ -0,0 +1,22 @@
+
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('wordnet')
+nltk.download('brown')
+nltk.download('universal_tagset')
+
+lemmatizer = WordNetLemmatizer()
+wordtags = nltk.ConditionalFreqDist((w.lower(), t) for w, t in nltk.corpus.brown.tagged_words(tagset="universal"))
+
+# Lemmatizer for english words
+def english_lemmatizer(term, pos):
+    pass
+
+# Tag part of speech 
+def english_pos(word):
+    print(list(wordtags[word.lower()].items()))
+
+
+english_pos("crashy")
