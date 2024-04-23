dff9cdbce3c44ea44ad72f5549ad85b1f44d63c7
elofg <elofg@kth.se>
2024-04-23 22:58:35 +0200

    Search logic for switching language

--- web_test/search.js
+++ web_test/search.js
@@ -51,13 +51,29 @@ const similar_words = {
 
 let last_get_similar_words_result = {};
 
+let language = 'swe';
+
+let switched_view = 'no';
+
+let done_search = 'no';
+
+let new_best_result = last_get_request_result;
+
 const getResults = (word) => {
+    done_search = 'yes';
     get_best_result(word);
     get_similar_results(word);
 
     // Show the search result containers
     document.getElementById("search-results").style.display = "block";
     document.getElementById("best-search-result").style.display = "block";
+
+    // Update display language if the toggle button has been clicked
+    if (language === 'eng') {
+        switchToEnglish();
+    } else {
+        switchToSwedish();
+    }
 };
 
 const get_best_result = (word) => {
@@ -70,6 +86,8 @@ const get_best_result = (word) => {
 
 const handle_button_parsing = (string_data) => {
     const data = JSON.parse(string_data);
+    new_best_result = data;
+    switched_view = 'yes';
     display_best_result(data);
 }
 
@@ -87,24 +105,44 @@ const display_best_result = (data) => {
 
     const bottomSection = document.createElement("div");
     bottomSection.classList.add("bottom-section");
+    if (language === 'swe') {
+        // Paragraphs for left section
+        create_paragraph("Svenskt lemma", data.swedishLemma, leftSection);
+        create_paragraph("Svenska böjningar", data.swedishInflections, leftSection);
 
-    // Paragraphs for left section
-    create_paragraph("Svenskt lemma", data.swedishLemma, leftSection);
-    create_paragraph("Svenska böjningar", data.swedishInflections, leftSection);
+        // Paragraphs for right section
+        create_paragraph("Engelskt lemma", data.englishLemma, rightSection);
+        create_paragraph("Engelska böjningar", data.englishInflections, rightSection);
 
-    // Paragraphs for right section
-    create_paragraph("Engelskt lemma", data.englishLemma, rightSection);
-    create_paragraph("Engelska böjningar", data.englishInflections, rightSection);
+        create_paragraph("Ordklass", data.pos, bottomSection);
+        create_paragraph("Alternativa översättningar", data.alternativeTranslations, bottomSection);
 
-    create_paragraph("Ordklass", data.pos, bottomSection);
-    create_paragraph("Alternativa översättningar", data.alternativeTranslations, bottomSection);
-
-     // Paragraphs for bottom section
-    // Logic for switching between källor and källa
-    if (data.source.length === 1) {
-        create_paragraph("Källa", data.source, bottomSection)
+        // Paragraphs for bottom section
+        // Logic for switching between källor and källa
+        if (data.source.length === 1) {
+            create_paragraph("Källa", data.source, bottomSection)
+        } else {
+            create_paragraph("Källor", data.source, bottomSection)
+        }
     } else {
-        create_paragraph("Källor", data.source, bottomSection)
+        // Paragraphs for left section
+        create_paragraph("Swedish lemma", data.swedishLemma, leftSection);
+        create_paragraph("Swedish inflections", data.swedishInflections, leftSection);
+
+        // Paragraphs for right section
+        create_paragraph("English lemma", data.englishLemma, rightSection);
+        create_paragraph("English inflections", data.englishInflections, rightSection);
+
+        create_paragraph("Part-of-speech", data.pos, bottomSection);
+        create_paragraph("Alternative translations", data.alternativeTranslations, bottomSection);
+
+        // Paragraphs for bottom section
+        // Logic for switching between källor and källa
+        if (data.source.length === 1) {
+            create_paragraph("Source", data.source, bottomSection)
+        } else {
+            create_paragraph("Sources", data.source, bottomSection)
+        }
     }
 
     // Append sections to container
@@ -128,7 +166,7 @@ const display_similar_results = () => {
     similar_words_container.innerHTML = "";
 
     const header = document.createElement("h2");
-    header.innerHTML = "Sökträffar:"
+    header.innerHTML = language === 'swe' ? "Sökträffar:" : "Search results:";
     similar_words_container.appendChild(header);
 
     create_button("sv", last_get_request_result, similar_words_container);
@@ -176,3 +214,124 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+// Function to change text content to English
+const switchToEnglish = () => {
+    // Change logo text
+    document.querySelector('.logo-main').textContent = 'STUNDA - ';
+    document.querySelector('.logo-sub').textContent = 'Swedish Technical University Network for Data Terms';
+    
+    // Change info text
+    document.querySelector('.info-text p').textContent = 'Stunda is a network for Sweden\'s technical universities aimed at promoting efficient technical communication in Swedish within higher education, primarily by working with discipline-specific terminology.';
+    
+    // Change button text
+    document.querySelector('.search-button').textContent = 'Search';
+
+    // Change placeholder text
+    document.getElementById('search-input').setAttribute('placeholder', 'Swedish or English data term');
+
+    if (done_search === 'yes') {
+        // Change header text
+        document.querySelector('#search-results h2').textContent = 'Search results:';
+
+        // Translate content within best-search-result
+        const bestSearchResult = document.getElementById('best-search-result');
+        const paragraphs = bestSearchResult.querySelectorAll('p');
+
+        if (switched_view === 'yes') {
+            show = new_best_result;
+        } else {
+            show = last_get_request_result;
+        }
+        paragraphs.forEach(paragraph => {
+            // Translate each paragraph content
+            if (paragraph.textContent.includes('Svenskt lemma')) {
+                paragraph.innerHTML = '<strong>Swedish lemma:</strong> ' + show.swedishLemma;
+            } else if (paragraph.textContent.includes('Svenska böjningar')) {
+                paragraph.innerHTML = '<strong>Swedish inflections:</strong> ' + show.swedishInflections.join(', ');
+            } else if (paragraph.textContent.includes('Engelskt lemma')) {
+                paragraph.innerHTML = '<strong>English lemma:</strong> ' + show.englishLemma;
+            } else if (paragraph.textContent.includes('Engelska böjningar')) {
+                paragraph.innerHTML = '<strong>English inflections:</strong> ' + show.englishInflections.join(', ');
+            } else if (paragraph.textContent.includes('Ordklass')) {
+                paragraph.innerHTML = '<strong>Part-of-speech:</strong> ' + show.pos;
+            } else if (paragraph.textContent.includes('Alternativa översättningar')) {
+                paragraph.innerHTML = '<strong>Alternative translations:</strong> ' + show.alternativeTranslations.join(', ');
+            } else if (paragraph.textContent.includes('Källor')) {
+                paragraph.innerHTML = '<strong>Sources:</strong> ' + show.source.join(', ');
+            }else if (paragraph.textContent.includes('Källa')) {
+                paragraph.innerHTML = '<strong>Source:</strong> ' + show.source;
+            }
+        });
+        const similarSearchHeader = document.getElementById('search-results');
+        const header = similarSearchHeader.querySelectorAll('h2');
+        header.innerHTML = 'Search results:';
+    }
+};
+
+// Function to change text content to Swedish
+const switchToSwedish = () => {
+    // Change logo text
+    document.querySelector('.logo-main').textContent = 'STUNDA - ';
+    document.querySelector('.logo-sub').textContent = 'Sveriges tekniska universitets nätverk för datatermer';
+
+    // Change info text
+    document.querySelector('.info-text p').textContent = 'Stunda är ett nätverk för Sveriges tekniska universitet med syfte att verka för effektiv fackspråklig kommunikation på svenska inom högre utbildning, främst genom att arbeta med disciplinspecifik terminologi.';
+    
+    // Change button text
+    document.querySelector('.search-button').textContent = 'Sök';
+
+    // Change placeholder text
+    document.getElementById('search-input').setAttribute('placeholder', 'Svensk eller engelsk dataterm');
+
+    if (done_search === 'yes'){
+        // Change header text
+        document.querySelector('#search-results h2').textContent = 'Sökträffar:';
+
+        // Translate content within best-search-result
+        const bestSearchResult = document.getElementById('best-search-result');
+        const paragraphs = bestSearchResult.querySelectorAll('p');
+        
+        if (switched_view === 'yes') {
+            show = new_best_result;
+        } else {
+            show = last_get_request_result;
+        }
+        paragraphs.forEach(paragraph => {
+            // Translate each paragraph content
+            if (paragraph.textContent.includes('Swedish lemma')) {
+                paragraph.innerHTML = '<strong>Svenskt lemma:</strong> ' + show.swedishLemma;
+            } else if (paragraph.textContent.includes('Swedish inflections')) {
+                paragraph.innerHTML = '<strong>Svenska böjningar:</strong> ' + show.swedishInflections.join(', ');
+            } else if (paragraph.textContent.includes('English lemma')) {
+                paragraph.innerHTML = '<strong>Engelskt lemma:</strong> ' + show.englishLemma;
+            } else if (paragraph.textContent.includes('English inflections')) {
+                paragraph.innerHTML = '<strong>Engelska böjningar:</strong> ' + show.englishInflections.join(', ');
+            } else if (paragraph.textContent.includes('Part-of-speech')) {
+                paragraph.innerHTML = '<strong>Ordklass:</strong> ' + show.pos;
+            } else if (paragraph.textContent.includes('Alternative translations')) {
+                paragraph.innerHTML = '<strong>Alternativa översättningar:</strong> ' + show.alternativeTranslations.join(', ');
+            } else if (paragraph.textContent.includes('Sources')) {
+                paragraph.innerHTML = '<strong>Källor:</strong> ' + show.source.join(', ');
+            }else if (paragraph.textContent.includes('Source')) {
+                paragraph.innerHTML = '<strong>Källa:</strong> ' + show.source;
+            }
+        });
+        const similarSearchHeader = document.getElementById('search-results');
+        const header = similarSearchHeader.querySelectorAll('h2');
+        header.innerHTML = 'Sökträffar:';
+    }
+};
+
+const language_toggle = () => {
+    const languageToggle = document.getElementById('language-toggle');
+    if (languageToggle.alt === 'swe') {
+        switchToSwedish();
+        language = 'swe';
+        languageToggle.alt = 'eng';
+    } else {
+        switchToEnglish();
+        language = 'eng';
+        languageToggle.alt = 'swe';
+    }
+}
