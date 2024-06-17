ee264ff1a9dce29c0a4e1be47a3338dd1244cea0
chickenThug <viggo.svardkrona@gmail.com>
2024-06-17 17:20:44 +0200

    fixed imports

--- src/se/stunda/logging/TermUploadServlet.java
+++ src/se/stunda/logging/TermUploadServlet.java
@@ -1,15 +1,12 @@
 package se.stunda.logging;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.nio.file.*;
+
 
 @MultipartConfig
 public class TermUploadServlet extends HttpServlet {
