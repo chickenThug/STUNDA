db9088cfe8e7e7f8b11a6426eac91366cae47c9b
elofg <elofg@kth.se>
2024-06-24 14:35:46 +0200

    Small fix

--- src/se/stunda/logging/TermUploadServlet.java
+++ src/se/stunda/logging/TermUploadServlet.java
@@ -7,10 +7,10 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.nio.file.*;
 
-
 @MultipartConfig
 public class TermUploadServlet extends HttpServlet {
     private static final String FILE_PATH = "/var/lib/stunda/terms/unprocessed.csv";
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -25,9 +25,11 @@ public class TermUploadServlet extends HttpServlet {
             }
 
             // Check if we have a file upload request
-            if (request.getContentType() != null && request.getContentType().toLowerCase().contains("multipart/form-data")) {
+            if (request.getContentType() != null
+                    && request.getContentType().toLowerCase().contains("multipart/form-data")) {
                 // Get the part of the request that contains the file
                 Part filePart = request.getPart("csvfile");
+                String source = request.getParameter("source");
 
                 if (filePart != null) {
                     try (BufferedReader reader = new BufferedReader(new InputStreamReader(filePart.getInputStream()))) {
@@ -38,8 +40,9 @@ public class TermUploadServlet extends HttpServlet {
                                 i++;
                                 continue;
                             }
-                            String writeString = line + "\n";
-                            Files.write(Paths.get(FILE_PATH), writeString.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                            String writeString = line + "," + source + "\n";
+                            Files.write(Paths.get(FILE_PATH), writeString.getBytes(), StandardOpenOption.CREATE,
+                                    StandardOpenOption.APPEND);
                         }
                     }
                 } else {
@@ -48,12 +51,11 @@ public class TermUploadServlet extends HttpServlet {
             } else {
                 response.getWriter().println("Invalid request content type!");
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.setContentType("text/plain");
             response.getWriter().write("error: " + e.getMessage());
         }
-        
+
     }
 }
