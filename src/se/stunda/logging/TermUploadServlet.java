c9073a3b049a8349aebeeb44e10cb976c21fe440
chickenThug <viggo.svardkrona@gmail.com>
2024-06-27 13:51:26 +0200

    now?

--- src/se/stunda/logging/TermUploadServlet.java
+++ src/se/stunda/logging/TermUploadServlet.java
@@ -63,22 +63,21 @@ public class TermUploadServlet extends HttpServlet {
             response.getWriter().write("error: " + e.getMessage());
         }
 
-        private void deleteDirectory(Path path) throws IOException {
-            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    Files.delete(file);
-                    return FileVisitResult.CONTINUE;
-                }
-    
-                @Override
-                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    Files.delete(dir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
     }
 
-    }
+    private void deleteDirectory(Path path) throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
 
-    
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
